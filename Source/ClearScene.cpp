cfbd6846473a906cb4e5da17b41f67a52b2f8438
Youetsu SATO <youetsux@gmail.com>
2025-03-30 12:54:45 +0900

    クリアシーンを調整中

--- Source/ClearScene.cpp
+++ Source/ClearScene.cpp
@@ -1,11 +1,31 @@
 #include "ClearScene.h"
 #include <DxLib.h>
 #include "Input.h"
+#include "../Source/Screen.h"
+
+
+namespace
+{
+	const int MESS_WIDTH{ 500 };
+}
+
 
 ClearScene::ClearScene()
-	:hImage_(-1)
+	:hImage_(-1),ImageRect_(0,0,0,0)
 {
+	hImage_ = LoadGraph("Assets/stageclear.png");
+	///1101 x 273  Pixels 
+	int w, h;
+	GetGraphSize(hImage_, &w, &h);
 
+	Point SSIZE{ Screen::WIDTH, Screen::HEIGHT };
+	Rect sRect{ 0, 0, SSIZE.x - 1, SSIZE.y - 1 };
+	Point center = sRect.GetCenter();
+	int wt, ht;
+	wt = (int)(MESS_WIDTH / 2);
+	ht = (int)((h * ((float)MESS_WIDTH / w)) / 2);
+	Rect drawRect{ center.x - wt, center.y - ht, center.x + wt , center.y + ht };
+	ImageRect_ = drawRect;
 }
 
 ClearScene::~ClearScene()
@@ -24,6 +44,7 @@ void ClearScene::Update()
 
 void ClearScene::Draw()
 {
-	DrawString(0, 0, "CLEAR SCENE", GetColor(255, 255, 255));
-	DrawString(100, 400, "Push Space Key To TITLE", GetColor(255, 255, 255));
+	DrawExtendGraph(ImageRect_.x, ImageRect_.y,ImageRect_.w, ImageRect_.h,  hImage_, TRUE);
+	//DrawString(0, 0, "CLEAR SCENE", GetColor(255, 255, 255));
+	//DrawString(100, 400, "Push Space Key To TITLE", GetColor(255, 255, 255));
 }
--- Source/ClearScene.h
+++ Source/ClearScene.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "../Library/SceneBase.h"
-
+#include "../globals.h"
 
 class ClearScene :
     public SceneBase
 {
     int hImage_;
+    Rect ImageRect_;
 public:
     ClearScene();
     ~ClearScene();
